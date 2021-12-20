d38b683156d14f17ae501244f5416b98a6b8ed79
White Tiger <dayley.zimmermann@gmail.com>
2021-12-20 14:32:08 -0500

    questions all filled in

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,1 +1,4 @@
 //Javascript for Code Quiz
+//setTimeout(()=> {
+// console.log("hello")}, 1000)    say hello after 1 second
+
--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -1,33 +1,34 @@
 // Here will be the javascript for the questions
-//
-var questions = [ {
-    title: "The condition in an if / else statement is enclosed within _____.",
+
+const quiz = [ 
+  {
+    question: "The condition in an if / else statement is enclosed within _____.",
     choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
     answer: "parentheses"
   },
   {
-    title: "The condition in an if / else statement is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parentheses"
+    question: "Arrays in JavaScript can be used to store _____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
   },
   {
-    title: "The condition in an if / else statement is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parentheses"
+    question: "Which of the following type of variable takes precedence over other if the names are the same? _____.",
+    choices: ["global variable", "local variable", "both of the above", "none of the above"],
+    answer: "local variable"
   },
   {
-    title: "The condition in an if / else statement is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parentheses"
+    question: "Which of the following function of String object combines the text of two strings and returns a new string? _____.",
+    choices: ["add()", "merge()", "concat()", "append()"],
+    answer: "concat()"
   },
   {
-    title: "The condition in an if / else statement is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parentheses"
+    question: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array? _____.",
+    choices: ["last()", "put()", "push()", "none of the above"],
+    answer: ""
   },
   {
-    title: "The condition in an if / else statement is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parentheses"
+    question: "Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array? _____.",
+    choices: ["push()", "join()", "pop()", "map()"],
+    answer: "map()"
   },
 ];
