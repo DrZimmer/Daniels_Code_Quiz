c89926f9d097f8f063cf8bbd68bc79c5720e5714
White Tiger <dayley.zimmermann@gmail.com>
2021-12-20 13:59:42 -0500

    set up initial questions in question.js

---
+++ assets/JS/questions.js
@@ -0,0 +1,33 @@
+// Here will be the javascript for the questions
+//
+var questions = [ {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parentheses"
+  },
+];
